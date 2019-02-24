15d9327f6194b75f1179fbd44f1e65b81f190b49
Santiago <shinca12@eafit.edu.co>
2019-02-24 14:37:50 -0500

    affine forward

--- assignment2/cs231n/layers.py
+++ assignment2/cs231n/layers.py
@@ -20,15 +20,11 @@ def affine_forward(x, w, b):
     - out: output, of shape (N, M)
     - cache: (x, w, b)
     """
-    out = None
-    ###########################################################################
-    # TODO: Implement the affine forward pass. Store the result in out. You   #
-    # will need to reshape the input into rows.                               #
-    ###########################################################################
-    pass
-    ###########################################################################
-    #                             END OF YOUR CODE                            #
-    ###########################################################################
+    N, *d = x.shape
+    D, M = w.shape
+    x = x.reshape(N, D)
+
+    out = x @ w + b 
     cache = (x, w, b)
     return out, cache
 
---
+++ assignment2/cs231n/own-test.py
@@ -0,0 +1,16 @@
+import layers
+import numpy as np
+
+num_inputs = 2
+input_shape = (4, 5, 6)
+output_dim = 3
+
+input_size = num_inputs * np.prod(input_shape)
+weight_size = output_dim * np.prod(input_shape)
+
+x = np.linspace(-0.1, 0.5, num=input_size).reshape(num_inputs, *input_shape)
+w = np.linspace(-0.2, 0.3, num=weight_size).reshape(np.prod(input_shape), output_dim)
+b = np.linspace(-0.3, 0.1, num=output_dim)
+
+
+out, _ = layers.affine_forward(x, w, b)
