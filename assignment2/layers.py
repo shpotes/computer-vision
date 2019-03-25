5d8e1b315b5efa34e2ab12f16d256396d601c2aa
shpotes <shinca12@eafit.edu.co>
2019-03-25 02:52:43 -0500

    LeNet done! TODO:   * Custom training loop   * AlexNet   * VGG16   * ResNet   * Inception

--- assignment2/layers.py
+++ assignment2/layers.py
@@ -1,14 +1,16 @@
+import tensorflow as tf
+
 class SubPooling(tf.keras.layers.Layer):
     def __init__(self, features, sz=[1, 2, 2, 1], st=[1, 2, 2, 1],
                  pad='VALID', **kwargs):
         super(SubPooling, self).__init__(**kwargs)
-        self.sz=[1, 2, 2, 1]
-        self.st=[1, 2, 2, 1]
-        self.pad='VALID'
+        self.sz=sz
+        self.st=st
+        self.pad=pad
         self.w = tf.Variable(tf.ones(features))
         self.b = tf.Variable(tf.zeros(features))
         
     def call(self, x):
-        S = tf.nn.avg_pool2d(input=x, ksize=self.sz,
+        S = tf.nn.avg_pool2d(input=x, ksize=self.sz, 
                              strides=self.st, padding=self.pad)
         return tf.nn.sigmoid(self.w * S + self.b)
